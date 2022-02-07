69035cffdb834bfb1242c901c675db576ac4a25e
rvbytes_local <52675110+rvbytes@users.noreply.github.com>
2022-02-07 20:53:49 +0530

    working model 1

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-const friends = ['Ross','Rachel','Monica','Chandler','Joey','Phoebe','Regina Phalange','Ken Adams','Mike','Crap Bag','Princess Consuela'];
-const crime = [
-    'was on a break',
-    'doesn\'t share food',
-    'wished they could help but didn\'t want to',
-    'makes jokes when they\'re uncomfortable',
-    'bought an expensive pair of boots',
-    'got off the plane',
-    'was backpacking across Western Europe',
-    'don\'t even have a pla'
-];
-const chat = [
-    'get a good massage',
-    'visit the Natural History museum',
-    'check the offers at Bloomingdale\'s',
-    'watch Die Hard I again',
-    'put the furnitures back in place',
-    'wish for a million dollars',
-    'order 2 pizzas',
-    'pivot'
-]
-
-const giveIntro = () => {
-    return `Hi, I'm ${friends[Math.floor(Math.random()*friends.length)]}'s detective friend.`;
-}
-
-const crimeCommitted = () => {
-    return `I suspect someone ${crime[Math.floor(Math.random()*crime.length)]}.`;
-}
-
-const chatBeforeInv = () => {
-    return `Before we investigate, lets ${chat[Math.floor(Math.random()*chat.length)]}.`
+const messageData = {
+    friends : [
+        'Ross','Rachel','Monica','Chandler','Joey','Phoebe','Mike',
+        'Regina Phalange','Ken Adams','Crap Bag','Princess Consuela'
+    ],
+    crime : [
+        'was on a break',
+        'doesn\'t share food',
+        'wished they could help but didn\'t want to',
+        'makes jokes when they\'re uncomfortable',
+        'bought an expensive pair of boots',
+        'got off the plane',
+        'was backpacking across Western Europe',
+        'don\'t even have a pla'
+    ],
+    chat : [
+        'get a good massage',
+        'visit the Natural History museum',
+        'check the offers at Bloomingdale\'s',
+        'watch Die Hard I again',
+        'put the furnitures back in place',
+        'wish for a million dollars',
+        'order 2 pizzas',
+        'pivot'
+    ],
+    random(arrayLength){
+        return Math.floor(Math.random()*arrayLength);
+    },
+    giveIntro(){
+        return `Hi, I'm ${this.friends[this.random(this.friends.length)]}'s detective friend.`;
+    },
+    crimeCommitted(){
+        return `I suspect someone ${this.crime[this.random(this.crime.length)]}.`;
+    },
+    chatBeforeInv(){
+        return `Before we investigate, lets ${this.chat[this.random(this.chat.length)]}.`;
+    },
+    displayMessage(){
+        document.getElementById("message").innerHTML = this.giveIntro() +'</br>'+ this.crimeCommitted() +'</br>'+ this.chatBeforeInv();
+    }
 }
